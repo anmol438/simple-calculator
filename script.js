6311e122e0211a213a9a24245f384fa452c3e531
Anmol Gupta <50985412+anmol438@users.noreply.github.com>
2022-06-24 00:36:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,52 @@
 function operation() {
-    var oper = this.innerText;
+    var input = this.innerText;
+    var calculated = false;
 
-    // if(operator1 == null && (oper == '+' || oper == '-' || oper == '*' || oper == '/' || oper == '=')){
-    //     alert("Please enter a number first");
-    if (oper != '+' && oper != '-' && oper != '*' && oper != '/' && oper != '=') {
-        if (operand == null) {
-            if (operator1 == null) {
-                operator1 = oper;
-            } else {
-                operator1 += oper;
-            }
-            console.log('operator1 =' + operator1);
-            display.innerText = operator1;
+    if (input == 'AC') {
+        display.innerText = 0;
+        operand1 = '0';
+        operand2 = '';
+        operator = '+';
+        ans = '0';
+    } else if (input != '+' && input != '-' && input != '*' && input != '/' && input != '=') {
+
+        if (!calculated) {
+            operand2 += input;
         } else {
-            if (operator2 == null) {
-                operator2 = oper;
-            } else {
-                operator2 += oper;
-            }
-            console.log('operator2 =' + operator2);
-            display.innerText = operator2;
+            operand2 = input;
+            calculated = false;
         }
 
+        if (operator == '=') {
+            ans = operand2;
+        }
+
+        display.innerText = operand2;
+
     } else {
-        if (oper == '=') {
-            if (operator2 == null) {
-                if (operator1 != null) {
-                    ans = operator1;
-                } else if (ans == null) {
-                    alert("bsdk phle number daal")
-                } else {}
-            } else {
-                ans = (eval(operator1 + operand + operator2));
-                operator1 = ans;
-                operand = null;
-                operator2 = null;
+        calculated = true;
+        if (operand2 != '') {
+            if (operator != '=') {
+                ans = (eval(operand1 + operator + operand2));
             }
-            console.log(ans);
+            operator = input;
+
+            if (input != '=') {
+                operand1 = ans;
+            } else {
+                operand1 = '0';
+            }
             display.innerText = ans;
+            operand2 = '';
+
         } else {
-            if (operator1 == null) {
-                alert("bsdk phle number daal")
+            if (input != '=') {
+                operand1 = ans;
             } else {
-                operand = oper;
-                console.log('operand =' + operand);
-                display.innerText = oper;
+                operand1 = '0';
             }
+            operator = input;
+            display.innerText = ans;
         }
     }
 
@@ -54,10 +55,10 @@ function operation() {
 var display = document.getElementById("display");
 var buttons = document.getElementsByClassName("button");
 
-var operator1 = null;
-var operator2 = null;
-var operand = null;
-var ans = null;
+var operand1 = '0';
+var operand2 = '';
+var operator = '+';
+var ans = '0';
 
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', operation);
